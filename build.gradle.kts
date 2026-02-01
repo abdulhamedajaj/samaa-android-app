fc8cce7c1160e5ff46a58b27b50a42cd92637342
abdulhamedajaj <abodyaj12@gmail.com>
2026-02-01 06:43:49 +0300

    Add Android and Kotlin plugins to build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,1 +1,10 @@
+plugins {
+  id("com.android.application") version "9.0.0" apply false
+  id("com.android.library") version "9.0.0" apply false
+  kotlin("android") version "1.9.0" apply false
+}
+
+tasks.register("clean", Delete::class) {
+  delete(rootProject.buildDir)
+}
 
